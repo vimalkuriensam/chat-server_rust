eaa7614724bf5730494137f82623c1187c6423e6
unknown <vimalkuriensam@gmail.com>
2022-05-21 09:44:23 +0530

    read individual line to print

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,21 @@
-use tokio::net::TcpListener;
+use tokio::{
+    io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
+    net::TcpListener,
+};
 
 #[tokio::main]
 async fn main() {
-    let listener: TcpListener = TcpListener::bind("127.0.0.1:8000").await.unwrap();
-    let (socket, _addr) = listener.accept().await.unwrap();
+    let listener: TcpListener = TcpListener::bind("127.0.0.1:8080").await.unwrap();
+    let (mut socket, _addr) = listener.accept().await.unwrap();
+    let (reader, mut writer) = socket.split();
+    let mut reader = BufReader::new(reader);
+    let mut line = String::new();
+    loop {
+        let bytes_read = reader.read_line(&mut line).await.unwrap();
+        if bytes_read == 0 {
+            break;
+        }
+        writer.write_all(&line.as_bytes()).await.unwrap();
+        line.clear();
+    }
 }
